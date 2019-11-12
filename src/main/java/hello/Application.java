34068790c6fca6e887e89562aa076d819d1f2cee
jainmnsh <jainmnsh@yahoo.com>
2019-11-12 15:54:30 -0800

    first version

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,65 @@
+package hello;
+
+import java.io.IOException;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+import javax.jms.JMSException;
+
+
+@SpringBootApplication
+@EnableJms
+public class Application {
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) throws Exception
+  {
+    //LocalDateTime startTime = LocalDateTime.now();
+    logger.info("Main Application start [{}]");
+    SpringApplication.run(Application.class,args);
+
+    //new Application().Start();
+    //SpringApplication.run(Application.class,args);
+  //  LocalDateTime endTime = LocalDateTime.now();
+
+    logger.info("Main application end [{}]");
+
+  }
+//  public void Start() throws  Exception
+//  {
+//    logger.info("Starting application now");
+//    initialize();
+//
+//    jmsClient.message();
+//  }
+//
+//  private void setDefaultExceptionHandler() {
+//    Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+//      public void uncaughtException(Thread t, Throwable e) {
+//        logger.error("Uncaught Exception in thread [{}]", t.getName(), e);
+//        System.exit(1);  //exception
+//      }
+//    });
+//  }
+//  private void initialize() throws JMSException, IOException {
+//    logger.debug("Starting the initialize process");
+//    setDefaultExceptionHandler();
+//    //commandLine = cli;
+//    logger.debug("Creating JMS");
+//    jmsClient = new JmsQueueClient();
+//    jmsClient.connect();
+//    logger.debug("Connected to JMS");
+//    logger.debug("Creating rest client");
+//
+//    // logger.debug("Creating JMX");
+//    // jmxClient = new JMXServiceClient(commandLine.getJmsClientConfiguration());
+//    // jmxClient.connect();
+//    // logger.debug("Connected to JMX");
+//  }
+}
---
+++ src/main/java/hello/Listener.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+
+@Component
+public class Listener {
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  @Autowired
+  private Producer producer;
+
+  @JmsListener(destination = "planner-test")
+  public void receiveMessage(final Message jsonMessage) throws JMSException {
+    String messageData = null;
+    System.out.println("Received message " + jsonMessage);
+    if(jsonMessage instanceof TextMessage) {
+      TextMessage textMessage = (TextMessage)jsonMessage;
+      messageData = textMessage.getText();
+      logger.debug("Message receved [{}]",messageData);
+    }
+    producer.sendMessage("planner-test-out", messageData);
+  }
+
+}
